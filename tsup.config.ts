d42fa80ad980a50fec4d76ccc70db11841e985f4
Shahrad Elahi <shahrad@litehex.com>
2025-08-04 20:03:58 +0100

    refactor(deps): Replace `lodash` with `lodash-es`

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig([
   {
     clean: true,
     dts: true,
+    minify: true,
+    keepNames: true,
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     target: 'esnext',
